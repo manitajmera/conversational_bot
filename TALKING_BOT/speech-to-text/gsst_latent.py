f0e2b9c0bb631f11e85013ed4a96f3751ae63487
manit ajmera <manitajmera@gmail.com>
2020-07-08 12:38:51 +0530

    Create gsst_latent.py

---
+++ TALKING_BOT/speech-to-text/gsst_latent.py
@@ -0,0 +1,13 @@
+import speech_recognition as sr
+import time
+r = sr.Recognizer()
+audio_file = sr.AudioFile('sample.aiff')
+
+def get_transcript(audio_file=audio_file):
+  with audio_file as source:
+    audio = r.record(source)
+  result = r.recognize_google(audio)
+  return result
+
+if __name__ == "__main__":
+  print(get_transcript())
